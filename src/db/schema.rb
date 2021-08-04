f5a73a092cbe54aae821bd124da6673f68d2726b
genmaisan <fujifujiseisei@gmail.com>
2021-08-04 17:01:58 +0900

    rubocopエラーのためコード修正

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_08_03_100735) do
-
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
--- src/spec/models/user_spec.rb
+++ src/spec/models/user_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   it "name と email を持ったユーザーを登録できること" do
     user = User.new(
       name: "John",
       email: "john@example.com",
-      )
-     expect(user).to be_valid
+    )
+    expect(user).to be_valid
   end
 end
