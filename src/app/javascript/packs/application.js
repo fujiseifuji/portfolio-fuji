9e76cffa1641bb6e7a8d0810d561055a6383b230
genmaisan <fujifujiseisei@gmail.com>
2021-09-17 15:50:51 +0900

    bootstrap周りの修正

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -10,15 +10,4 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "jquery"
-import "bootstrap"
-import "../src/application"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("jquery")
