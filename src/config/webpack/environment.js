a91c47044d79b66476f7fbb3f2a1be54cfc4a9b2
genmaisan <fujifujiseisei@gmail.com>
2021-09-22 16:07:56 +0900

    アイコンの追加

--- src/config/webpack/environment.js
+++ src/config/webpack/environment.js
@@ -3,7 +3,7 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 
 const webpack = require('webpack')
-environment.plugins.append(
+environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
