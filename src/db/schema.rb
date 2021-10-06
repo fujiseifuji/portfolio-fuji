05f01e8814f1de73fb11d4eab4399990bdca1744
genmaisan <fujifujiseisei@gmail.com>
2021-10-07 03:53:24 +0900

    circleciでrubocopエラー発生のため修正

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -52,7 +52,9 @@ ActiveRecord::Schema.define(version: 20_210_914_031_643) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
