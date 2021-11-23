21edc911aadece2298b01a7450cbc59128769b65
genmaisan <fujifujiseisei@gmail.com>
2021-11-24 02:00:47 +0900

    アプリ名を変更

--- src/spec/helpers/application_helper_spec.rb
+++ src/spec/helpers/application_helper_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe ApplicationHelper, type: :helper do
     context "page_titleが空白の場合" do
       let(:page_title) { "" }
 
-      it { is_expected.to eq("BSK search") }
+      it { is_expected.to eq("ストバスぽっと") }
     end
 
     context "page_titleがnilの場合" do
       let(:page_title) { nil }
 
-      it { is_expected.to eq("BSK search") }
+      it { is_expected.to eq("ストバスぽっと") }
     end
 
     context "page_titleが存在する場合" do
       let(:page_title) { "sample" }
 
-      it { is_expected.to eq("sample - BSK search") }
+      it { is_expected.to eq("sample - ストバスぽっと") }
     end
   end
 end
